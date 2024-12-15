55916264d34ba42a4e3a515ccf56289f9c70c98b
c4fiber <qudcjf153@gmail.com>
2024-12-15 22:57:15 +0900

    공통함수 추가

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -3,24 +3,18 @@ import pendulum
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from common.common_func import get_sftp
 
 # catchup: 누락되어 있는 작업을 진행을 할 것인가 결정 -> true일 경우 차례차례 돌리는게 아니라 한번에 돌린다.
 # 객체id와 task id는 같게 가져가는 표준으로 가겠다.
 
 with DAG(
-    dag_id="dags_python_operator",
+    dag_id="dags_python_import_func",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
-    def select_fruit():
-        ''' 주어진 과일 중 랜덤하게 하나를 뽑아서 출력합니다. '''
-        fruit = ['APPLE', 'BANANA', 'ORANGE', 'AVOCADO']
-        rand_int = random.randint(0, 3)
-        print(fruit[rand_int])
-    py_t1 = PythonOperator(
-        task_id='py_t1',
-        python_callable=select_fruit
+    task_get_sftp = PythonOperator(
+        task_id='task_get_sftp',
+        python_callable=get_sftp
     )
-
-    py_t1
